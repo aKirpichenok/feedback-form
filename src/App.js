fda2610670d3651bef4d26bc7d182abb9831c671
aKirpichenok <a.kirpichenok99@gmail.com>
2022-06-24 16:32:17 +0300

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="email" />
+        <input type="phone" />
+        <input type="date" />
+        <textarea placeholder="Text" />
+      </form>
+    </div>
+  )
+}
+
+export default App;
